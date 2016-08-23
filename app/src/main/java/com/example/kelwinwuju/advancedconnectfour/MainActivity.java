6398f794b47e31862ece6300291647e7588c512b
kelwinwu <kelwinwucs@gmail.com>
2016-08-23 19:07:12 -0400

    Added first intent

--- app/src/main/java/com/example/kelwinwuju/advancedconnectfour/MainActivity.java
+++ app/src/main/java/com/example/kelwinwuju/advancedconnectfour/MainActivity.java
@@ -1,24 +1,31 @@
 package com.example.kelwinwuju.advancedconnectfour;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
+import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements View.OnClickListener{
 
     TextView advancedText;
     TextView connectFourText;
     TextView connectFourText2;
     Typeface fontOne;
     Typeface fontTwo;
+
+    //public Button playButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
+
 
         advancedText = (TextView)findViewById(R.id.advancedtext);
         fontOne = Typeface.createFromAsset(getAssets(), "fonts/blow.ttf");
@@ -31,5 +38,20 @@ public class MainActivity extends Activity {
         connectFourText2 = (TextView)findViewById(R.id.connectfourtext2);
         connectFourText2.setTypeface(fontTwo);
 
+
+        ImageButton playButton = (ImageButton)findViewById(R.id.playButton);
+        playButton.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v){
+        switch(v.getId()){
+            case R.id.playButton:
+                //Do your stuff here
+                Intent playIntent = new Intent(MainActivity.this, PlaySetupActivity.class);
+                startActivity(playIntent);
+                break;
+        }
     }
 }
